a50a7e4c9623e7dca8a8e996fb91dc71d1212dbe
archit-mriirs <architsinghal_mriirs@outlook.com>
2022-12-07 15:29:18 +0530

    Added manifest file

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -8,8 +8,6 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => {
-                console.log('Opened the cache')
-
                 return cache.addAll(urlsToCache)
             })
     )
